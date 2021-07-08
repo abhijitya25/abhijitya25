092c68214ccf0e7fe3849195892794b68de9ac1f
abhijitya25 <49483160+abhijitya25@users.noreply.github.com>
2021-07-08 16:40:12 +0530

    countdown timer

---
+++ countdown/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { useState, useEffect, useRef } from 'react';
+
+const App = (props) => {
+
+  const { initialMinute = 5, initialSeconds = 0 } = props;
+  const [minutes, setMinutes] = useState(initialMinute);
+  const [seconds, setSeconds] = useState(initialSeconds);
+  const [start,setStart]=useState(false);   
+  
+  const handleStop=()=>{
+    setMinutes(initialMinute)
+    setSeconds(initialSeconds)
+  }
+
+  const handleOnclick=()=>{
+    setStart(!start)
+  }
+
+  useEffect(() => {
+    if (start===true){
+    let myInterval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds => seconds - 1);
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+           clearInterval(myInterval)
+        } else {
+          setMinutes(minutes => minutes - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000)
+    return () => {
+      clearInterval(myInterval);
+    };
+  }
+});
+ 
+  return (
+    <div className='loading'><div>
+      <h2>{minutes}m {seconds}s</h2>
+      </div>
+      <div>
+      <button className="btn" onClick={handleOnclick}>{start ? "Stop":"Start"}</button>
+      <button className="btn" onClick={handleStop}>Reset</button>
+      </div>
+      {/* {minutes === 0 && seconds === 0
+        ? null
+        : <h1> {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+      } */}
+    </div>
+  )
+}
+
+export default App
